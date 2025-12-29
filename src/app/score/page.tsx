4e47d6465d2393c7d64edf3b5b85d82663c67402
Vatsal Vyas <vatsalvyas4@gmail.com>
2025-12-29 10:53:01 +0000

    React is not defined
    src/app/score/page.tsx (106:26) @ <unknown>
    
      10

--- src/app/score/page.tsx
+++ src/app/score/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { useState, useEffect, useMemo } from 'react';
 import { database } from '@/lib/db';
 import { useUser } from '@/contexts/UserContext';
